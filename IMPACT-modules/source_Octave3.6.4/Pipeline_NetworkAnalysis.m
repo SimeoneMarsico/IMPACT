6f3ca8f89aeb3f3e1fe4a192a1af468609cbd366
SimeoneMarsico <persego@gmail.com>
2014-03-07 17:46:30 +0000

    octave-addition

---
+++ IMPACT-modules/source_Octave3.6.4/Pipeline_NetworkAnalysis.m
@@ -0,0 +1,123 @@
+ function Pipeline_NetworkAnalysis
+% loaded input file parameters should look like:
+% do_seed_nodes,do_module_search,do_randomizations,do_statistics,SIMIL_THR,MIN_NUMB_PROF,N_RAND,N_MAX
+% e.g. 1,1,1,1,0.7,2,5000,4
+% where do_seed_nodes     = binary, [0,1], do the import (convert input text files into matlab environment files)
+%       do_module_search  = binary, [0,1], do the search procedure on sets
+%       do_randomizations = binary, [0,1], do the randomizations
+%       do_statistics     = binary, [0,1], calculate p-values and export results based on the randomization results
+%       SIMIL_THR = number, [0-1], similarity threshold (Pearson correlation coefficient) for set search and randomizations
+%       MIN_NUMB_PROF = number, if >= 1 it is the number of profiles above similarity threshold required for module search (e.g. 2); 
+%                               if < 1 it is the fraction of profiles above threshold. 
+%       N_RAND = integer number, e.g. 1000, number of randomization for each set
+%       N_MAX = integer number, e.g. 4, number of bins on the number-of-profiles distribution for the randomizations
+%
+% OUTPUT: .mat and .txt files in the same folder as input files
+
+%% SETTING BASIC PARAMETER FOR ENVIRONMENT AND SEARCH
+
+[param_file param_file_path] = uigetfile('*.txt', 'Load paramters file','.');
+a = dlmread([param_file_path param_file]);
+do_seed_nodes = a(1)
+do_module_search = a(2)
+do_randomizations = a(3)
+do_statistics = a(4)
+SIMIL_THR = a(5)
+MIN_NUMB_PROF = a(6)
+N_RAND = a(7)
+N_MAX = a(8)
+ 
+% set path and file names
+[networkFileIN my_path] = uigetfile('*.txt', 'Load network interaction data','.');
+networkFileIN = [my_path networkFileIN];
+[ExperimentalDataFileIN my_path] = uigetfile('*.txt', 'Load phenotypic data',my_path);
+ExperimentalDataFileIN  = [my_path ExperimentalDataFileIN];
+[temp1 base_name] = fileparts(ExperimentalDataFileIN);
+
+networkFile                 = [strtok(networkFileIN,'.') '.mat'];
+ExperimentalDataFile        = [strtok(ExperimentalDataFileIN,'.') '.mat'];
+SeedNodeFile                = [my_path base_name '_seed-file_prof-' num2str(MIN_NUMB_PROF) '_thr-' num2str(SIMIL_THR*100) '.mat'];
+VectorSeedNodeStartingFile  = [my_path base_name '_seed-list_prof-' num2str(MIN_NUMB_PROF) '_thr-' num2str(SIMIL_THR*100) '.txt'];
+ModuleResultsFile           = [my_path base_name '_res-search-file_prof-' num2str(MIN_NUMB_PROF) '_thr-' num2str(SIMIL_THR*100) '.mat'];
+OutputFileDistr             = [my_path base_name '_p-distr-file_prof-' num2str(MIN_NUMB_PROF) '_thr-' num2str(SIMIL_THR*100) '.mat'];
+OutputFileStats             = [my_path base_name '_res-stat-file_prof-' num2str(MIN_NUMB_PROF) '_thr-' num2str(SIMIL_THR*100) '.mat'];
+ExportTextFile              = [my_path base_name '_output-export-' num2str(MIN_NUMB_PROF) '_thr-' num2str(SIMIL_THR*100) '.txt'];
+ListExportName              = [my_path base_name '_export_list-' num2str(MIN_NUMB_PROF) '_thr-' num2str(SIMIL_THR*100)];
+
+if(do_seed_nodes || do_module_search)
+    do_it = 1;
+    if ( exist(networkFile, 'file') == 2)   % .mat file with the same name already exist
+        yes_no = input('A network file for this input file has already been created. \nDo you want to overwrtie it? y/n : ','Import network') ;
+        if (strcmp(yes_no,'n') == 1)
+            do_it = 0;
+        end
+    end
+    if (do_it)
+        networkFile = convert_text_mat(1, 1, networkFileIN);
+    end
+    
+    do_it = 1;
+    if ( exist(ExperimentalDataFile, 'file') == 2)   % .mat file with the same name already exist
+        yes_no = input('A phenotypic file for this input file has already been created. \nDo you want to overwrtie it? y/n : ','Import profiles');
+        if (strcmp(yes_no,'n') == 1)
+            do_it = 0;
+        end
+    end
+    if (do_it)
+        ExperimentalDataFile = convert_text_mat(1, 0, ExperimentalDataFileIN);
+    end
+end
+%% FILE IMPORT, SEED NODE AND SEED PATTERN SELECTION
+
+if (do_seed_nodes)
+    [PotentialSeedNodeDef, SeedNodePatternsOriginal, SeedNodeProfilesOriginal] = RunTOPmoduleSeedSelection(networkFile, ExperimentalDataFile, 1, 0.8, SeedNodeFile, 2);
+    vector=ones(size(PotentialSeedNodeDef));
+    dlmwrite(VectorSeedNodeStartingFile,vector), clear vector;
+end
+
+%%  MODULE SEARCH
+% input files required (string variables inside script)
+%       netowrk file (e.g., 'HPRDinVivo_Intact_Kegg15092009.mat')
+%       phenotypic data file (e.g., 'AllOligo40ProfilesEnsembl_oligSymb_Nozscore.mat')
+%       seed node file (e.g., 'VectorFileCombinedNetSetseed08_numprof2_TESTJuly2012.txt')
+%       seed pattern profile file (e.g., 'SeedNodeHPRDinVivo_numProf2_setNode08_TESTJuly2012.mat')
+% output files 
+%       module search result file (e.g., 'ResultsCombinedNetSetNodes2Prof_07simil_OCT2012.mat')
+% parameters defined
+%       SIMIL_THR = similarity theshold (e.g. 0.7);
+%       MIN_NUMB_PROF = minimal number of required simialr profiles (e.g. 2);
+if(do_module_search)
+    module_search (networkFile, ExperimentalDataFile, VectorSeedNodeStartingFile, SeedNodeFile, ModuleResultsFile, SIMIL_THR, MIN_NUMB_PROF)
+end
+
+%% DISTRIBUTION CALCULATION 
+% input files required (string variables inside script)
+%       module search result file (e.g., 'ResultsCombinedNetSetNodes2Prof_08simil_OCT2012.mat')        
+%       seed pattern profile file (e.g., 'SeedNodeHPRDinVivo_numProf2_setNode08_TESTJuly2012.mat')
+% output files
+%       probability distribution file for used seeds (e.g., 'P_seedPattern_ModeProfileOCT2012_1-100.mat')
+
+if (do_randomizations)
+    Probabilities_distribution_calculation(ModuleResultsFile,SeedNodeFile,OutputFileDistr, MIN_NUMB_PROF, SIMIL_THR, N_RAND, N_MAX);
+end
+%% MODULE STATISTICAL ASSESSMENT (module fusion, p-values, ROC curves)
+% input files required (string variables inside script)
+%       module search result file (e.g., 'ResultsCombinedNetSetNodes2Prof_08simil_OCT2012.mat')        
+%       probability distribution file for used seeds (e.g., 'P_seedPattern_ModeProfileOCT2012_1-100.mat')
+%       reference set (e.g., ReferenceSets_GOExperimental_GOAll_POP.mat)
+% output files
+%       p-value file (e.g., 'RESULTS_2prof_07simil_OCT2012.mat')
+
+if (do_statistics)
+    warning('off', 'MATLAB:nchoosek:LargeCoefficient');
+    ModuleFusion_PvalueCalculation(ModuleResultsFile,OutputFileDistr,OutputFileStats,SIMIL_THR,N_MAX);
+    warning('on', 'MATLAB:nchoosek:LargeCoefficient');
+    th = 0.1;
+    [list_all_network, list_all_modules, list_sig_modules] = PrintResultsToFile_Modules (OutputFileStats,ExportTextFile, th);
+    name1 = [ListExportName '_allnet.txt'];
+    name2 = [ListExportName '_allmod.txt'];
+    name3 = [ListExportName '_allsignificant.txt'];
+    cell2csv(name1,list_all_network);
+    cell2csv(name2,list_all_modules);
+    cell2csv(name3,list_sig_modules);
+end
---
+++ IMPACT-sets/source_Octave3.6.4/Pipeline_SetAnalysis.m
@@ -0,0 +1,167 @@
+function Pipeline_SetAnalysis
+% loaded input file parameters should look like:
+% do_seed_nodes,do_module_search,do_randomizations,do_statistics,SIMIL_THR,MIN_NUMB_PROF,N_RAND,N_MAX
+% e.g. 1,1,1,1,0.7,2,5000,4
+% where do_seed_nodes     = binary, [0,1], do the import (convert input text files into matlab environment files)
+%       do_module_search  = binary, [0,1], do the search procedure on sets
+%       do_randomizations = binary, [0,1], do the randomizations
+%       do_statistics     = binary, [0,1], calculate p-values and export results based on the randomization results
+%       SIMIL_THR         = number, [0-1], similarity threshold (Pearson correlation coefficient) for set search and randomizations
+%       MIN_NUMB_PROF     = not used, kept only for compatibility with Pipeline_NetworkAnalysis.m (IMPACT-modules); it must be passed to the function (it may be used in future implementations);
+%       N_RAND            = integer number, e.g. 1000, number of randomization for each set
+%       N_MAX             = integer number, e.g. 4, number of bins on the number-of-profiles distribution for the randomizations
+%
+% OUTPUT: .mat and .txt files in the same folder as input files
+
+%% SETTING BASIC PARAMETER FOR ENVIRONMENT AND SEARCH
+[param_file param_file_path] = uigetfile('*.txt', 'Load paramters file','.');
+a = dlmread([param_file_path param_file]);
+do_import_mapping = a(1)
+do_set_search = a(2)
+do_randomizations = a(3)
+do_statistics = a(4)
+SIMIL_THR = a(5)
+MIN_NUMB_PROF = a(6)
+N_RAND = a(7)
+N_MAX = a(8)
+
+[set_interaction_file my_path] = uigetfile('*.txt', 'Load set interaction data','.');
+set_interaction_file = [my_path set_interaction_file];
+[experimental_pheotypic_file my_path] = uigetfile('*.txt', 'Load phenotypic data',my_path);
+experimental_pheotypic_file  = [my_path experimental_pheotypic_file];
+my_slash = my_path(end);
+[temp1 base_name] = fileparts(experimental_pheotypic_file);
+
+output_file_pheno_mapped = [my_path base_name '_pheno-mapped.mat'];
+output_file_pheno = [my_path 'MultiparametricDataAllprofile-08_ImportedTEST.mat'];  % useless remove later on
+RandomizationsFolder = [my_path 'RandomizationsFolder' my_slash];
+if not(isdir(RandomizationsFolder))
+    mkdir(RandomizationsFolder)
+end
+OutputRandFile   = [RandomizationsFolder 'Randomizations_thr-' num2str(100*SIMIL_THR) '_n-rand-' num2str(N_RAND) '.mat'];
+OutputSearchFile = [my_path base_name 'res-search_thr-' num2str(100*SIMIL_THR) '.mat'];
+OutputResultFile = [my_path base_name '_res-final_thr-' num2str(100*SIMIL_THR) '.mat'];
+ExportTextFile   = [my_path base_name '_output_export-' num2str(100*SIMIL_THR) '.txt'];
+ListExportName   = [my_path base_name '_export_list-'   num2str(SIMIL_THR*100)];
+
+%% import data from txt file and create mat file to use for next step analysis
+if(do_import_mapping)
+    do_it = 1;
+    if ( exist(output_file_pheno_mapped, 'file') == 2)   % .mat file with the same name already exist
+        button = questdlg('A mapping file for this input file has already been created. Do you want to overwrtie it?','Import/Map profiles') ;
+        if (strcmp(button,'No') == 1)
+            do_it = 0;
+        end
+    end
+    if (do_it)
+        [flag_input] = import_map_data(set_interaction_file,experimental_pheotypic_file, output_file_pheno_mapped, output_file_pheno);
+    end
+end
+% Load phenotypic data mapped on the interaction data (sets, e.g. protein complexes from CORUM)
+if (do_set_search || do_randomizations || do_statistics)
+    load(output_file_pheno_mapped)
+end
+%% Set-base analysis: search enriched pattern in complexes 
+if(do_set_search)
+    [Realselected,RealselectedPnotSwap,RealreferenceP,RealmatrixP,RealMaxNUmProf,RealNumGenesSelected,RealTotNumProf,RealSelGenesID,RealSelectedOligos]=searchComplexProfiles2(newComplexes,newComplexesID,newComplexesSymbol,SIMIL_THR);
+    save (OutputSearchFile, 'Real*');
+else
+    load(OutputSearchFile);
+end
+
+%% RANDOMIZATION PROCEDURE
+% this steps allow to estimate the statistical significance by computing a
+% pvalue using a permutation strategy
+% 1 - run searching procedure over a randomly assembled set
+if (do_randomizations)
+    
+    % all screens profiles
+    numProfilesPerGene_unique = zeros(1,length(allOligoProf));
+    for i=1:length(allOligoProf)
+        numProfilesPerGene_unique(i) = size(allOligoProf{i},1);
+    end
+    % creation of 
+    % Bins : set of bins containing gene profiles according to their number of profiles 
+    % vectRandTemp : total count of genes in each bin.
+    
+    temp = sort(numProfilesPerGene_unique);
+    for i = 1: N_MAX
+        N(i) = temp(i * floor(length(temp)   /(N_MAX+1)));
+    end
+    N = unique(N);
+    N_MAX = min(N_MAX, length(N));
+    for i = 1 : N_MAX
+        if i == 1
+            Bins{i}=allOligoProf(numProfilesPerGene_unique<=N(i));
+            vectRandTemp(i,:)=[length(find(numProfilesPerGene_unique<=N(i))) 1];
+        elseif i < N_MAX
+            Bins{i}=allOligoProf(numProfilesPerGene_unique > N(i-1) & numProfilesPerGene_unique <= N(i));
+            vectRandTemp(i,:)=[length(find(numProfilesPerGene_unique > N(i-1) & numProfilesPerGene_unique <= N(i))) i];
+        else
+            Bins{i}=allOligoProf(numProfilesPerGene_unique>=N(i));
+            vectRandTemp(N_MAX,:)=[length(find(numProfilesPerGene_unique>=N(i))) N_MAX];
+        end
+    end
+    
+    SetProfiles = newComplexes;
+    BinnedSetinfo_PhenotypicDataFile = [my_path 'BinnedDatasetForRandomizations.mat'];
+    save(BinnedSetinfo_PhenotypicDataFile, 'SetProfiles', 'Bins', 'vectRandTemp');
+
+    RunRandomizationWithBinningSet(BinnedSetinfo_PhenotypicDataFile,OutputRandFile,N_RAND,SIMIL_THR, N);
+
+end
+
+%% STATISTICS CALCULATION BASED ON RANDOMIZATIONS
+if (do_statistics)
+
+    % Load results of randomizations steps
+    Randomizations_selectedGenes=[];
+    Randomizations_selectedProfiles=[];
+    tempFilenames=dir(RandomizationsFolder);
+    tempFilenames2={tempFilenames.name};
+    FilenamesInd=strmatch('Randomiz',tempFilenames2);
+    Filenames=tempFilenames2(FilenamesInd);
+    clear tempFilenames tempFilenames2 FilenamesInd
+
+    for i=1:length(Filenames)
+        load([RandomizationsFolder Filenames{i}])
+        Randomizations_selectedGenes=[Randomizations_selectedGenes;Rand_SelectedGenes];
+        Randomizations_selectedProfiles=[Randomizations_selectedProfiles;Rand_selectedProfiles];
+    end
+
+    % P-value computations
+    pvalueGenes = pvalue_fromFrequenciesVersionNovember09(RealreferenceP,RealNumGenesSelected,Randomizations_selectedGenes);
+    pvalueProfiles = pvalue_fromFrequenciesVersionNovember09(RealreferenceP,RealMaxNUmProf,Randomizations_selectedProfiles);
+
+    % preparation and saving output files
+    selectedGenesAnalysis = {};
+    selectedPvalues = [];
+    for i=1:length(RealSelGenesID)
+        if ~isempty(RealreferenceP{i})
+            if (~iscell(RealSelGenesID{i}))
+                if (length(RealSelGenesID{i} == 1))
+                     RealSelGenesID{i} = cellstr(newComplexesID{i});
+                else
+                    RealSelGenesID{i} = cellstr(RealSelGenesID{i});
+                end
+            end
+            for j=1:length(RealSelGenesID{i})
+                if ~isempty(RealSelGenesID{i}{j})
+                    selectedGenesAnalysis = [selectedGenesAnalysis; RealSelGenesID{i}{j}];
+                    selectedPvalues = [selectedPvalues; pvalueGenes(i)'];
+                end
+            end
+        end
+        clear ngenes values
+    end
+    
+    save(OutputResultFile, 'pvalue*', 'Real*', 'newComplexes*', 'allProteinSet*');
+    th = 0.1;
+    [list_all_sets, list_sel_sets, list_sig_sets] = PrintResultsToFile_Complexes (OutputResultFile,ExportTextFile, th);
+    name1 = [ListExportName '_all-sets.txt'];
+    name2 = [ListExportName '_sel-sets.txt'];
+    name3 = [ListExportName '_sign-sets.txt'];
+    cell2csv(name1,list_all_sets);
+    cell2csv(name2,list_sel_sets);
+    cell2csv(name3,list_sig_sets);
+end
